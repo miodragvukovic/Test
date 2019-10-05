687b12e6171e1a05f601b2f0acb92a007a45b568
miodrag.vukovic <35942671+miodragvukovic@users.noreply.github.com>
2019-10-05 18:41:37 +0200

    Navbar adjustment

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 let navbar = document.querySelector('.navigation');
-document.querySelector('.dropdown-trigger').addEventListener('click', function(){
-	this.classList.contains('active') ? this.classList.remove('active') : this.classList.add('active');
+const navbarHeight = navbar.offsetHeight
+
+document.querySelector('.dropdown-trigger').addEventListener('click', () => {
 	navbar.classList.contains('active') ? navbar.classList.remove('active') : navbar.classList.add('active');
 });
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', () => {
 	let top = pageYOffset;
-	if ( top > navbar.offsetHeight ) {
+	console.log(top)
+	console.log(navbar.offsetHeight)
+	if ( top >= navbarHeight ) {
 		navbar.classList.remove('absolute')
 		setTimeout(function(){
 			navbar.classList.add('fixed')
-		}, 300);
-		
+		}, 50);
 	} else {
 		navbar.classList.remove('fixed')
-
 		navbar.classList.add('absolute')
 	}
 });
