507f1f5b24cb1b5ff3409cac2580472f519e5ac9
miodrag.vukovic <35942671+miodragvukovic@users.noreply.github.com>
2019-10-06 01:08:39 +0200

    Latest Fixed navbar adjustments

--- script.js
+++ script.js
@@ -3,24 +3,36 @@
 let navbar = document.querySelector('.navigation');
 const navbarHeight = navbar.offsetHeight;
 
+// CONDITION FOR NAVBAR SHOWING ON PAGE LOAD
+
+if ( pageYOffset > navbarHeight ) {
+	navbar.classList.add('fixed');
+}
+
 // DROPDOWN TRIGGER EVENT
 
 document.querySelector('.dropdown-trigger').addEventListener('click', () => {
 	navbar.classList.contains('active') ? navbar.classList.remove('active') : navbar.classList.add('active');
+	if ( pageYOffset > navbarHeight )
+		navbar.classList.add('fixed');
 });
 
 // WINDOW SCROLL EVENT
 
 window.addEventListener('scroll', () => {
 	let top = pageYOffset;
-	if ( top > navbarHeight ) {
-		navbar.classList.remove('absolute');
-		setTimeout(function(){
-			navbar.classList.add('fixed');
-		}, 300);
-	} else {
-		navbar.classList.remove('fixed');
-		navbar.classList.add('absolute');
+	if ( !navbar.classList.contains('active') ) {
+		if ( top >= navbarHeight ) {
+			navbar.classList.remove('absolute');
+			setTimeout(function(){
+				navbar.classList.add('fixed');
+			}, 300);
+		} else if ( top == 0 ) {
+			navbar.classList.add('absolute');
+			setTimeout(function(){
+				navbar.classList.remove('fixed');
+			}, 300);
+		}
 	}
 });
 
